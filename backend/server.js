dc02bf20c44ae07700706b4ea04ca3ceb9643324
AshishChaudhari07 <ashishchaudhari19112003@gmail.com>
2024-10-10 19:31:58 +0530

    cors origin change

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ connectCloudinary();
 
 //middel ware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://medical-scheduling-system-admin.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 // api endpoint
 app.use('/api/admin',adminRouter)
