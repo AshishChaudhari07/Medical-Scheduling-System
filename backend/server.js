383298a1cc5e8edced9288cb97b4f914c44371a2
AshishChaudhari07 <ashishchaudhari19112003@gmail.com>
2024-11-30 22:27:35 +0530

    first-commit

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,33 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv';
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudniary.js';
-import adminRouter from './routes/adminRoute.js';
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudniary.js'
+import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js';
 import userRouter from './routes/userRoute.js';
 
-// App configuration
-const app = express();
-dotenv.config();
-const port = process.env.PORT || 4000;
 
-// Database and Cloudinary connections
+// app config
+const app = express()
+dotenv.config();
+const port = process.env.PORT || 4000
 connectDB();
 connectCloudinary();
 
-// Middleware
-app.use(express.json());
-app.use(cors({
-    origin: 'https://medical-scheduling-system-frontend.onrender.com', // Restrict origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+//middel ware
+app.use(express.json())
+app.use(cors())
 
-// API Endpoints
-app.use('/api/admin', adminRouter);
-app.use('/api/doctor', doctorRouter);
-app.use('/api/user', userRouter);
+// api endpoint
+app.use('/api/admin',adminRouter)
+app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
 
-app.get('/', (req, res) => {
-    res.send("API Working Great");
-});
+app.get('/',(req,res)=>{
+    res.send("Api Working Great")
+})
 
-// Server listener
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+app.listen(port,()=>{
+    console.log(`server start on http://localhost:${port}`);
+})
